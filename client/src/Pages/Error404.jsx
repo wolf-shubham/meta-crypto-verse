5a107d39770e363c02e73e4bfdd7bc4ce53b3f85
Shubham Sharma <sharma.shubham0191@gmail.com>
2022-05-03 11:51:57 +0530

    error page

--- client/src/Pages/Error404.jsx
+++ client/src/Pages/Error404.jsx
@@ -1,8 +1,29 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+import LeftNavbar from '../components/LeftNavbar'
 
 const Error404 = () => {
     return (
-        <div>Error404</div>
+        <div style={{ display: 'flex' }}>
+            <LeftNavbar />
+            <div
+                style={{
+                    flex: 9.5,
+                    height: '90vh',
+                    backgroundColor: 'white',
+                    overflowY: 'scroll',
+                    paddingLeft: '1rem',
+                    paddingTop: '5px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '1.5rem',
+                    textAlign: 'center'
+                }}
+            >
+                <Typography variant="h3" >Error 404.<br />Page Not Found ...</Typography>
+            </div>
+        </div>
     )
 }
 
